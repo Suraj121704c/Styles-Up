e89482862477094849498d4cc2fbbe09b48bff46
9802HEMENSAN <hmahilange9802@gmail.com>
2023-03-29 17:49:10 +0530

    folder-structure-created

--- paytm-mall/src/index.js
+++ paytm-mall/src/index.js
@@ -4,11 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      < ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Provider>
   </BrowserRouter>
 );
 
