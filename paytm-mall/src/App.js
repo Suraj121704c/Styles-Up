9755686a847321749f8b0679f359ffc14899cbfe
Suraj <nhb668912@gmail.com>
2023-03-30 12:09:03 +0530

    day3

--- paytm-mall/src/App.js
+++ paytm-mall/src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import AllRoutes from "./Components/AllRoutes";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      
+      <AllRoutes />
+      <Navbar />
     </div>
   );
 }
