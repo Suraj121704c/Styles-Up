3b62f6f5123f7726fb28ffb84d79ad1e6ebc0e3f
rk28284 <112754760+rk28284@users.noreply.github.com>
2023-03-30 12:29:51 +0530

    fw21_1141-day-2

--- paytm-mall/src/App.js
+++ paytm-mall/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import AllRoutes from './Components/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-      
+
     </div>
   );
 }
--- paytm-mall/src/Components/AllRoutes.jsx
+++ paytm-mall/src/Components/AllRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { SingleUserPage } from '../Admin/Pages/SingleUserPage'
 
 const AllRoutes = () => {
   return (
     <div> 
         <Routes>
             <Route path="/admin" element={<AdminHome/>}></Route>
+            <Route path='/fashion/:id' element={<SingleUserPage/>}/>
         </Routes>
     </div>
   )
