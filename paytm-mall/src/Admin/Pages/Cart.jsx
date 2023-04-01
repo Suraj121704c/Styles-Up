01812d8a1cd149da43172a607c786a96c2b40f88
rk28284 <112754760+rk28284@users.noreply.github.com>
2023-04-02 01:01:09 +0530

    singlrUser page completed

--- paytm-mall/src/Admin/Pages/Cart.jsx
+++ paytm-mall/src/Admin/Pages/Cart.jsx
@@ -8,11 +8,26 @@ const Cart = () => {
   console.log(products);
   return (
     <div>
-
+    {products.map((el)=>{
+      return (
+          <div key={el.id}>
+            
+      <img src={el.image1} alt="image1" />
+            </div>
+    )})}
 
 
     </div>
   )
 }
+// "image1": "https://assetscdn1.paytm.com/images/catalog/product/J/JE/JEWVIGHNAHARTA-VIGH1196979EA0CDD9/1562708763888_0..jpg?imwidth=282&impolicy=hq",
+// "Brand": "Vighnaharta",
+// "title": "Silver Brass Ring",
+// "price": 249,
+// "MRP": "1163",
+// "Category": "jewellary",
+// "rating": 4.7,
+// "quantitiy": 1,
+// "id": 1
 
 export default Cart
