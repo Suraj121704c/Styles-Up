538b6b54f7d0b521aee5b9e1cc8228ebafb83894
9802HEMENSAN <hmahilange9802@gmail.com>
2023-04-01 23:29:20 +0530

    fw21_0767_day_5 admin completed

--- paytm-mall/src/Components/AllRoutes.jsx
+++ paytm-mall/src/Components/AllRoutes.jsx
@@ -8,21 +8,24 @@ import Glasses from "../Pages/Products/Glasses";
 import Jewellery from "../Pages/Products/Jewellery";
 import Decoration from "../Pages/Products/Decoration";
 import { AutoMobiles } from "../Pages/Products/autoMobiles";
+ 
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/admin" element={<Admin/>}></Route>
       <Route path='/fashion/:id' element={<SingleUserPage />} />
         <Route path="/electronic" element={<Products />} />
         <Route path="/jewellery" element={<Jewellery />} />
         <Route path="/glasses" element={<Glasses />} />
         <Route path="/decoration" element={<Decoration />} />
         <Route path="/autoMobiles" element={<AutoMobiles />} />
+        <Route path="/admin" element={<Admin/>}></Route>
       </Routes>
   );
 };
 
 export default AllRoutes;
+ 
+
 
