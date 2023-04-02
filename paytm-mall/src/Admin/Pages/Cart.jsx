1e59814446929cb2c6e871305180114e7cb751f1
rk28284 <112754760+rk28284@users.noreply.github.com>
2023-04-02 12:57:08 +0530

    total  completed

--- paytm-mall/src/Admin/Pages/Cart.jsx
+++ paytm-mall/src/Admin/Pages/Cart.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 import {
+  Grid, GridItem,HStack,
   Box,
   Button,
   Image,
@@ -14,13 +16,61 @@ import {
   Text,
   useDisclosure
 } from "@chakra-ui/react";
-import { useGet } from '../../hooks/useGet'
+
 import { Link } from "react-router-dom";
-const url = `https://growup.onrender.com/orders`
+import { useEffect } from 'react';
+import axios from 'axios';
+// const url = `https://growup.onrender.com/orders`
 const Cart = () => {
-  const { isLoading, products, serverError } = useGet(url);
+ 
   const { isOpen, onOpen, onClose } = useDisclosure();
-  console.log(products);
+  const [data, setData] = useState([])
+  const [total, setTotal] = useState(0);
+
+  const getCartItems =async ()=> {
+    try {
+      return axios({
+       method : "get" ,
+       url : `https://growup.onrender.com/orders`,
+      }).then((res) =>{ 
+        console.log(res.data)
+        setData(res.data)})
+    } catch (error) {
+      console.log("err") ; 
+    }
+}  
+
+const handleDelete = async (id)=>{
+  return axios({
+   method : "delete",
+   url : `https://growup.onrender.com/orders/${id}`
+  }).then(()=> getCartItems());
+}
+
+useEffect(() => {
+  getCartItems()
+  
+}, [])
+
+// Total
+useEffect(()=>{
+  let Total=0;
+  data?.forEach((item)=>(
+   Total+= (+item.price) * (+item.quantity)
+  ))
+   setTotal(Total)
+   console.log(typeof Total)
+},[data])
+
+const handleQuantity=(id, quantity , val)=>{
+  data.map((item, index)=> (
+  item.id === id ?   (quantity = quantity + val ) :  quantity 
+  ))
+axios.patch(`https://growup.onrender.com/orders/${id}`,{
+  quantity : quantity
+}).then(()=> getCartItems())
+ 
+}
   return (
     <div>
 
@@ -59,18 +109,50 @@ const Cart = () => {
             </Text>
           </Box>
 
-          <Box name="cart_card">
-            {products.map((el) => {
-              return (
-                <div key={el.id}>
+          <Grid  gap={5}>
+         {
+          data?.map((cart)=>(
+            <GridItem key={cart.id}  m={"auto"} display = "flex" boxSizing='border-box'  >
+               <HStack gap={4}> 
+               <Box> 
+              <Image src={cart.image1} alt="cart image" h={150} w= {150} />
+              </Box>
+              <Box> 
+               <Text fontSize={18}>{cart.Category}</Text>
+              
+               <Text fontSize={15}>discount :{`${cart.price}`}</Text>
+         
+               {/* Buttons Quantity */}
+               </Box>
+               </HStack>
+               <HStack gap={10}> 
+               <Box ml={10}>
+                 
+                <Button  bg={"red.700"} isDisabled = {cart.quantity===1} onClick={()=> handleQuantity(cart.id , cart.Quantity ,  -1)}>-</Button>
 
-                  <img src={el.image1} alt="image1" />
-                  <h3>{el.title}</h3>
-                </div>
-              )
-            })}
+                <Button isDisabled>{cart.quantity}</Button>
 
-          </Box>
+                <Button bg={"green.700"} 
+                onClick={()=> handleQuantity(cart.id , cart.quantity , 1)}>+</Button>
+                  {/* <Select  onChange ={ handleChange(cart.id, cart.Quantity, ) }>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                  </Select> */}
+                </Box>
+                <Box>
+                <Text fontSize={16}>{`₹ ${cart.price * cart.quantity}`}</Text>
+                </Box>
+                <Box>
+                <Button onClick={()=> handleDelete(cart.id)}> Remove</Button>
+                </Box>
+                </ HStack>
+            </GridItem>
+          ))
+         }
+          
+     </Grid>
         </Box>
         <Box
           w={{ base: "100%", sm: "80%", md: "35%" }}
@@ -173,10 +255,10 @@ const Cart = () => {
               p="10px"
             >
               <Text fontWeight="400" fontSize={"1rem"}>
-                Amount Payable
+                Amount Payable : {total}
               </Text>
               <Text color={"#F25B22"} fontWeight="400" fontSize={"1rem"}>
-                ₹
+                ₹ {total}
               </Text>
             </Box>
             <Box>
