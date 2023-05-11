63f2bc243d962a65b3550160b9e9ea8478a75c1b
Suraj Singh <112753795+Suraj121704c@users.noreply.github.com>
2023-05-11 20:11:14 +0530

    products

--- paytm-mall/src/Pages/Products/Jewellery.jsx
+++ paytm-mall/src/Pages/Products/Jewellery.jsx
@@ -12,7 +12,15 @@ import {
   Text,
   Button,
   VStack,
+  Spinner,
   CircularProgress,
+  CardBody,
+  Stack,
+  Heading,
+  Divider,
+  CardFooter,
+  ButtonGroup,
+  Card,
 } from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation, useSearchParams } from "react-router-dom";
@@ -37,7 +45,9 @@ const Jewellery = () => {
   const [Brand, setBrand] = useState(initialBrand || []);
   const initialRating = searchParams.getAll("rating");
   const [rating, setRating] = useState(initialRating || []);
-  const { isLoading ,isError } = useSelector((store)=> store.electronicsReducer )
+  const { isLoading, isError } = useSelector(
+    (store) => store.electronicsReducer
+  );
 
   const handleSort = (e) => {
     setOrder(e.target.value);
@@ -94,7 +104,7 @@ const Jewellery = () => {
       Category: searchParams.getAll("Category"),
       Brand: searchParams.getAll("Brand"),
       rating: searchParams.getAll("rating"),
-      q : searchParams.get("q")
+      q: searchParams.get("q"),
     },
   };
 
@@ -103,7 +113,6 @@ const Jewellery = () => {
   }, [location.search]);
 
   // console.log(jewellery)
- 
 
   return (
     <>
@@ -119,33 +128,28 @@ const Jewellery = () => {
                 <Checkbox
                   colorScheme="green"
                   value={"jewellary"}
-                  onChange={handleCategory}
-                >
+                  onChange={handleCategory}>
                   jewellary
                 </Checkbox>
                 <Checkbox
                   colorScheme="green"
                   value={"lahenga"}
-                  onChange={handleCategory}
-                >
+                  onChange={handleCategory}>
                   lahenga
                 </Checkbox>
                 <Checkbox
                   colorScheme="green"
                   value={"kurtas"}
-                  onChange={handleCategory}
-                >
+                  onChange={handleCategory}>
                   kurtas
                 </Checkbox>
                 <Checkbox
                   colorScheme="green"
                   value={"kurti"}
-                  onChange={handleCategory}
-                >
+                  onChange={handleCategory}>
                   kurti
                 </Checkbox>
               </VStack>
-              <label>Brand</label>
               <VStack mb="20px" alignItems="flex-start">
                 <Text fontWeight="bold" mb="3px" color="blackAlpha.600">
                   Brands
@@ -153,57 +157,49 @@ const Jewellery = () => {
                 <Checkbox
                   colorScheme="green"
                   value={"Vighnaharta"}
-                  onChange={handleBrand}
-                >
+                  onChange={handleBrand}>
                   Vighnaharta
                 </Checkbox>
                 <Checkbox
                   colorScheme="green"
                   value={"Mahi"}
-                  onChange={handleBrand}
-                >
+                  onChange={handleBrand}>
                   Mahi
                 </Checkbox>
                 <Checkbox
                   colorScheme="green"
                   value={"Efulgenz"}
-                  onChange={handleBrand}
-                >
+                  onChange={handleBrand}>
                   Efulgenz
                 </Checkbox>
                 <Checkbox
                   colorScheme="green"
                   value={"Rajkanya"}
-                  onChange={handleBrand}
-                >
+                  onChange={handleBrand}>
                   Rajkanya
                 </Checkbox>
 
                 <Checkbox
                   colorScheme="green"
                   value={"SILVER SHINE"}
-                  onChange={handleBrand}
-                >
+                  onChange={handleBrand}>
                   SILVER SHINE
                 </Checkbox>
 
                 <Checkbox
                   colorScheme="green"
                   value={"HAPPY STONING"}
-                  onChange={handleBrand}
-                >
+                  onChange={handleBrand}>
                   HAPPY STONING
                 </Checkbox>
 
                 <Checkbox
                   colorScheme="green"
                   value={"SAINOOR"}
-                  onChange={handleBrand}
-                >
+                  onChange={handleBrand}>
                   SAINOOR
                 </Checkbox>
               </VStack>
-              <label>Ratings</label>
               <VStack mb="20px" alignItems="flex-start">
                 <Text fontWeight="bold" mb="3px" color="blackAlpha.600">
                   Ratings
@@ -211,15 +207,13 @@ const Jewellery = () => {
                 <Checkbox
                   colorScheme="green"
                   value={4.3}
-                  onChange={handleRating}
-                >
+                  onChange={handleRating}>
                   3⋆⋆ and above
                 </Checkbox>
                 <Checkbox
                   colorScheme="green"
                   value={4.7}
-                  onChange={handleRating}
-                >
+                  onChange={handleRating}>
                   4⋆⋆⋆⋆ and above
                 </Checkbox>
               </VStack>
@@ -236,16 +230,14 @@ const Jewellery = () => {
                 "::-webkit-scrollbar": {
                   display: "none",
                 },
-              }}
-            >
+              }}>
               <Flex
                 justifyContent="space-between"
                 alignItems="center"
                 p="5px"
                 bg="#e2e8f0"
                 border="1px"
-                borderColor="gray.400"
-              >
+                borderColor="gray.400">
                 <Text>STYLISH FASHION</Text>
                 <Flex>
                   <Flex alignItems="center">
@@ -260,8 +252,7 @@ const Jewellery = () => {
                     borderColor="black"
                     ml="4px"
                     p="0px"
-                    onChange={handleSort}
-                  >
+                    onChange={handleSort}>
                     <option value="">Sort By Price</option>
                     <option value="asc">Price : low to high</option>
                     <option value="desc">Price : high to low</option>
@@ -274,13 +265,27 @@ const Jewellery = () => {
                   base: "repeat(1, 1fr)",
                   sm: "repeat(2, 1fr)",
                   md: "repeat(3, 1fr)",
-                  lg: "repeat(4, 1fr)",
-                }}
-              >
-                {jewellery.length > 0 &&
+                  lg: "repeat(3, 1fr)",
+                }}>
+                {jewellery.length === 0 ? (
+                  <Spinner
+                    thickness="4px"
+                    speed="0.65s"
+                    emptyColor="gray.200"
+                    color="blue.500"
+                    size="xl"
+                  />
+                ) : (
                   jewellery.map((el) => (
                     <Link to={`/products/${el.id}`} key={el.id}>
-                      <Box p="2" h="17rem" boxShadow="md" rounded="md">
+                      {/* <Box
+                        p="1"
+                        h="17rem"
+                        boxShadow= "rgba(6, 24, 44, 0.4) 0px 0px 0px 2px, rgba(6, 24, 44, 0.65) 0px 4px 6px -1px, rgba(255, 255, 255, 0.08) 0px 1px 0px inset"
+                        rounded="md"
+                        marginLeft={"3"}
+                        marginTop={"5"}
+                        >
                         <Image
                           src={el.image1}
                           alt={el.Brand}
@@ -301,17 +306,45 @@ const Jewellery = () => {
                           ₹{el.price}{" "}
                         </Text>
                         <Text as={"s"}>₹{el.MRP}</Text>
-                      </Box>
+                      </Box> */}
+                      <Card maxW="sm">
+                        <CardBody>
+                          <Image
+                            src={el.image1}
+                            alt="Green double couch with wooden legs"
+                            borderRadius="lg"
+                            height={"300px"}
+                          />
+                          <Stack mt="6" spacing="3">
+                            <Heading size="md">{el.Brand}</Heading>
+                            <Text>
+                              {el.title}
+                            </Text>
+                            <Text color="blue.600" fontSize="2xl">
+                              ₹{el.price}
+                            </Text>
+                          </Stack>
+                        </CardBody>
+                        <Divider />
+                        <CardFooter>
+                          <ButtonGroup spacing="2">
+                            <Button variant="ghost" colorScheme="blue">
+                              Add to cart
+                            </Button>
+                          </ButtonGroup>
+                        </CardFooter>
+                      </Card>
                     </Link>
-                  ))}
+                  ))
+                )}
               </Grid>
               <Center marginTop={"10px"}>
                 <Button
                   onClick={() => setPage(page - 1)}
-                  isDisabled={page === 1}
-                >
+                  isDisabled={page === 1}>
                   Previous
-                </Button>&nbsp;&nbsp;&nbsp;
+                </Button>
+                &nbsp;&nbsp;&nbsp;
                 <Button>{page}</Button>&nbsp;&nbsp;&nbsp;
                 <Button onClick={() => setPage(page + 1)}>Next</Button>
               </Center>
